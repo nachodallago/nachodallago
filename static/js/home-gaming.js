be7c8499425644ed036dbd29dbb6bcbd43e2478d
Nacho <dallagoignacio@gmail.com>
2021-09-01 00:12:49 -0300

    Gaming and cookie

--- static/js/home-gaming.js
+++ static/js/home-gaming.js
@@ -21,7 +21,7 @@ function getSteamInfo(){
     .then(data => {
         console.log(data)
         if(data.status=='online'){statusTxt=' <div class="gaming-online d-inline-block"></div> <span class="text-secondary">En línea</span>'}
-        if(data.status=='in-game'){statusTxt=' <div class="gaming-online d-inline-block"></div><br><small> <img src="'+data.in_game.icon+'"> Jugando a '+data.in_game.game+'</small>'}
+        if(data.status=='in-game'){statusTxt=' <div class="text-secondary"><div class="mb-2"><span class="gaming-online d-inline-block"></span> <span style="font-size:10px;font-weight:300">AHORA</span></div> <small> <img src="'+data.in_game.icon+'" class="rounded"> Jugando a '+data.in_game.game+'</small></div>'}
         if(data.status=='offline'){statusTxt='<span class="text-secondary">Actualmente desconectado</span>'}
         avatar.setAttribute('src',data.avatar);
         status_steam.innerHTML = statusTxt;
