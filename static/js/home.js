52ac15ff0ede6d6efd6279e119874443763e1393
Nacho <dallagoignacio@gmail.com>
2021-10-10 01:46:53 -0300

    Fix

--- static/js/home.js
+++ static/js/home.js
@@ -68,6 +68,24 @@ function getProjectInfo() {
                 autoplay: {
                     delay: 2500,
                     disableOnInteraction: false,
+                },
+                // Responsive breakpoints
+                breakpoints: {
+                    // when window width is >= 320px
+                    320: {
+                    slidesPerView: 1,
+                    spaceBetween: 20
+                    },
+                    // when window width is >= 480px
+                    480: {
+                    slidesPerView: 2,
+                    spaceBetween: 30
+                    },
+                    // when window width is >= 640px
+                    640: {
+                    slidesPerView: 3,
+                    spaceBetween: 40
+                    }
                 }
             });
             
