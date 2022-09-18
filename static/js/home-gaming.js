f9a248c002a6873b78cb7623657e00c871a6c286
Nacho <dallagoignacio@gmail.com>
2022-09-18 18:31:31 -0300

    better styles

--- static/js/home-gaming.js
+++ static/js/home-gaming.js
@@ -23,9 +23,9 @@ function getSteamInfo(){
     .then(response => response.json())
     .then(data => {
         console.log(data)
-        if(data.status=='online'){statusTxt=' <div class="gaming-online d-inline-block"></div> <span class="text-secondary">En línea</span>'}
-        if(data.status=='in-game'){statusTxt=' <div class="text-secondary"><div class="mb-2"><span class="gaming-online d-inline-block"></span> <span style="font-size:10px;font-weight:300">AHORA</span></div> <small> <img src="'+data.in_game.icon+'" class="rounded"> Jugando a '+data.in_game.game+'</small></div>'}
-        if(data.status=='offline'){statusTxt='<span class="text-secondary">Actualmente desconectado</span>'}
+        if(data.status=='online'){statusTxt=' <span class="badge bg-success"><div class="gaming-online d-inline-block"></div> <span class="text-white">En línea</span></span>'}
+        if(data.status=='in-game'){statusTxt='<span class="badge bg-success"> <div class="text-white"><div class="mb-2"><span class="gaming-online d-inline-block"></span> <span style="font-size:10px;font-weight:300">AHORA</span></div> <small> <img src="'+data.in_game.icon+'" class="rounded"> Jugando a '+data.in_game.game+'</small></div></span>'}
+        if(data.status=='offline'){statusTxt='<span class="badge bg-secondary shadow text-dark">Actualmente desconectado</span>'}
         avatar.setAttribute('src',data.avatar);
         status_steam.innerHTML = statusTxt;
         name_steam.innerHTML=data.name;
